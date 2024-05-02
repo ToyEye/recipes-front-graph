aacf1f8062567613e85906a6b61944e00c2e3f15
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-02 13:49:31 +0300

    add serve

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const client = new ApolloClient({
-    uri: "http://localhost:3001/graphql",
+    uri: "https://recipes-server-graph.onrender.com/graphql",
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
