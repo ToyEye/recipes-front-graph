3c0c55958824120ea4d0c25cb664398d46f82d2f
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-08 12:42:00 +0300

    change style for skeleton

--- app/lib/skeletons/RecipeDetailsSkeleton.tsx
+++ app/lib/skeletons/RecipeDetailsSkeleton.tsx
@@ -1,46 +1,42 @@
+import Section from "@/components/Section/Section";
 import React from "react";
 
 const RecipeDetailsSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <div className="animate-pulse">
-        <div className="bg-gray-300 h-96 w-full md:w-[720px] lg:w-[660px]"></div>
-      </div>
-      <div>
-        <div className="animate-pulse">
-          <div className="flex items-center mb-4">
-            <div className="bg-gray-300 h-8 w-8 rounded-full"></div>
-            <span className="ml-2 text-sm text-gray-500 dark:text-gray-400">
-              Loading...
-            </span>
-          </div>
-          <h2 className="text-2xl font-bold mb-2">Ingredients:</h2>
-          <ul className="list-disc pl-6 mb-4">
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-          </ul>
-          <h2 className="text-2xl font-bold mb-2">Instructions:</h2>
-          <ol className="list-decimal pl-6 mb-4">
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-            <li>Loading...</li>
-          </ol>
-          <div className="flex items-center mb-4">
-            <div className="bg-gray-300 h-8 w-8 rounded-full mr-2"></div>
-            <div>
-              <h3 className="text-lg font-bold">Loading...</h3>
-              <p className="text-gray-600">Author</p>
+    <Section>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="animate-pulse overflow-hidden">
+          <div className="bg-gray-300 h-96 w-full md:w-[720px] lg:w-[660px]"></div>
+        </div>
+        <div>
+          <div className="animate-pulse">
+            <div className="flex items-center mb-4">
+              <div className="bg-gray-300 h-8 w-24 rounded-full"></div>
+            </div>
+            <h2 className="text-2xl font-bold mb-2">Ingredients:</h2>
+            <ul className="list-disc pl-6 mb-4 flex flex-col gap-1">
+              <li className="bg-gray-300 rounded-xl"></li>
+              <li className="bg-gray-300 rounded-xl"></li>
+              <li className="bg-gray-300 rounded-xl"></li>
+            </ul>
+            <h2 className="text-2xl font-bold mb-2">Instructions:</h2>
+            <ol className="list-decimal pl-6 mb-4 flex flex-col gap-1">
+              <li className="bg-gray-300 rounded-xl"></li>
+              <li className="bg-gray-300 rounded-xl"></li>
+              <li className="bg-gray-300 rounded-xl"></li>
+              <li className="bg-gray-300 rounded-xl"></li>
+            </ol>
+            <div className="flex items-center mb-4">
+              <div className="bg-gray-300 h-8 w-8 rounded-full mr-2"></div>
+              <div>
+                <h3 className=" bg-gray-200 rounded-xl h-8 w-24"></h3>
+                <p className="text-gray-600">Author</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import RecipeDetailsSkeleton from "@/app/lib/skeletons/RecipeDetailsSkeleton";
 import RecipeCard from "@/components/RecipeCard/RecipeCard";
 import queries from "@/graphql/queries";
 import { useQuery } from "@apollo/client";
 import React from "react";
 
 const Recipe = ({ params }: { params: { id: string } }) => {
-  const { data } = useQuery(queries.GET_RECIPE_BY_ID, {
+  const { data, loading } = useQuery(queries.GET_RECIPE_BY_ID, {
     variables: { id: params.id },
   });
 
   console.log(data?.recipe);
-  return <div>{data && <RecipeCard {...data?.recipe} />}</div>;
+  return (
+    <div>
+      {data && <RecipeCard {...data?.recipe} />}
+      {loading && <RecipeDetailsSkeleton />}
+    </div>
+  );
 };
 
 export default Recipe;
