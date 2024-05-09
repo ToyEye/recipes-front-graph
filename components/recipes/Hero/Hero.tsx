8f2a55d3333e1c9b93e2532c9fa73948343294c5
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-09 13:34:46 +0300

    add img recipes page

--- components/recipes/Hero/Hero.tsx
+++ components/recipes/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import Heading from "@/components/Heading/Heading";
 import Section from "@/components/Section/Section";
 import RandomRecipe from "../RandomRecipe/RandomRecipe";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -11,10 +12,23 @@ const Hero = () => {
         tag="h1"
       />
 
-      <p className="text-gray-600 mb-8 md:text-xl">
+      <p className="text-gray-600 mb-3 md:text-xl">
         Discover a world of delicious recipes from around the globe.
       </p>
 
+      <Image
+        width={1600}
+        height={600}
+        src={"/img/recipes.avif"}
+        alt="recipe book"
+        className="mb-4 lg:mb-8"
+      />
+
+      <Heading
+        className="text-2xl font-medium  mb-8 md:text-5xl"
+        text="Random Recipes"
+        tag="h2"
+      />
       <RandomRecipe />
     </Section>
   );
